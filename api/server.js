e1254fbcb7f5c3985082a840b2aaf269540e06ea
AustinJHealy <56621614+AustinJHealy@users.noreply.github.com>
2020-10-14 22:34:34 -0400

    wrote SQL queries and imported budget db to server

--- api/server.js
+++ api/server.js
@@ -1,9 +1,9 @@
 const express = require("express");
-
+const budget = require("./budget");
 const db = require("../data/dbConfig.js");
 
 const server = express();
 
 server.use(express.json());
-
+server.use("/budget", budget);
 module.exports = server;
